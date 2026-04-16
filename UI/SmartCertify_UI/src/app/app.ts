782b300d2ce2ba61a77ba1581c3fb48c907ec628
bobettes1 <kenzelkdsouza@gmail.com>
2026-04-17 03:38:12 +0530

    Add pages for UI

--- UI/SmartCertify_UI/src/app/app.ts
+++ UI/SmartCertify_UI/src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { About } from "./pages/about/about";
+import { Header } from "./pages/header/header";
+import { Footer } from "./pages/footer/footer";
+import { Home } from "./pages/home/home";
+import { ContactUs } from "./pages/contact-us/contact-us";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, About, Header, Footer, Home, ContactUs],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ UI/SmartCertify_UI/src/app/pages/about/about.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  imports: [],
+  templateUrl: './about.html',
+  template: `<h1>About</h1>`,
+  styleUrl: './about.css',
+})
+export class About {}
