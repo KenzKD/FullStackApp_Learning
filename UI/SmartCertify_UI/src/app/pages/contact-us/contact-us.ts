a906cd13dc46abe99791573691ca23d93e1f12ef
bobettes1 <kenzelkdsouza@gmail.com>
2026-04-18 02:33:35 +0530

    Add Content to Pages

--- UI/SmartCertify_UI/src/app/pages/contact-us/contact-us.ts
+++ UI/SmartCertify_UI/src/app/pages/contact-us/contact-us.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './contact-us.html',
-  styleUrl: './contact-us.css',
+  styleUrl: './contact-us.css'
 })
-export class ContactUs {}
+export class ContactUs implements OnInit {
+  contactForm!: FormGroup;
+  userId=0;
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  triggerError() {
+    // Simulate a non-HTTP error
+    throw new Error('This is a simulated error');
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+    
+    }
+  }
+}
--- UI/SmartCertify_UI/src/app/pages/header/header.ts
+++ UI/SmartCertify_UI/src/app/pages/header/header.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
-  templateUrl: `./header.html`,
-  styleUrl: './header.css',
+  imports: [CommonModule, FormsModule, RouterModule],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
 })
-export class Header {}
+export class Header {
+
+}
